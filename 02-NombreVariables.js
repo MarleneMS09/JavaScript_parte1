1fd598d0b8f4af3700d92ede40a13c0b7fa7749c
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-06 20:01:48 -0600

    se agrego explicación de variables y nuevo archivo de convensiontipos.js

--- 02-NombreVariables.js
+++ 02-NombreVariables.js
@@ -1,4 +1,25 @@
-let nombreEmpleado= "José Perez";
+let
+
+
+mbreEmpleado= "José Perez";
 let dirección = "Av.105 -Calle 3";
 let costoDelPasaje= 1500;
 let tasaEmbarque= 50.5;
+
+const miNombre = "Leonardo";
+console.log("miNombre");
+//El resultado es que debe escribirse el texto miNombre
+
+/* Buenas prácticas con variables:
+-JS es un lenguaje sensible a mayúsculas y minúsculas.
+-Trabajando con las variables
+-Gestión de errores con NodeJS
+-JS: Lenguaje fuertemente tipado vs lenguaje libre
+-Js es lenguaje libre
+
+
+
+
+
+
+*/
---
+++ 03-BuenasPracticas.js
@@ -0,0 +1,18 @@
+le
+ nombreEmpleado = "José Peréz";
+//let nombreEmpleado= "Pedro Ramírez"; //se pone como comentario para que no marque error 
+let edadEmpleado=40; //interpretará que es un número
+
+console.log(nombreEmpleado);
+console.log(nombreEmpleado);
+/* aquí podríamos pensar que JS leerá la misma variable pero salen los dos nombres con espacio de manera diferente pero da error en la consola*/ 
+//incluso si lo coloco de la siguiente manera lo acepta
+
+/* 
+let edadEmpleado=40;
+console.log(nombreEmpleado);
+edadEmpleado= "cuarenta"
+console.log(nombreEmpleado); 
+
+
+*/
