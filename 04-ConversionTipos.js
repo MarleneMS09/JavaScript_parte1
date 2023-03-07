871855cb8939b3971c78975ef2c37c7db1ad8f1e
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-06 20:19:55 -0600

    se agregaron variables, se modificará para realizar ejercicios adicionales

--- 04-ConversionTipos.js
+++ 04-ConversionTipos.js
@@ -2,3 +2,14 @@
 //ALFANUMERICO
 const nombrePasajero = "Pedro Silva";
 console.log (nombrePasajero);
+
+//Númerico
+let valorBoleto= 1650;
+console.log(valorBoleto);
+
+//Lógicos (true,false)
+let boletoActivo =true;
+
+//Operaciones aritméticas
+let totalBoletos= 1700 + 65.45 + 100;
+console.log (totalBoletos);
