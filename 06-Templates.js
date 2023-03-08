62a7cac10971e049784d7fd24ec83cede1f102f2
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-07 18:14:37 -0600

    se agrego un archivo de templates sobre const & var

---
+++ 06-Templates.js
@@ -0,0 +1,9 @@
+const nombrePasajero = 'Leonardo';
+const apellidoPasajero= 'La cruz';
+
+let nombreCompleto= `El nombre completo es: ${nombrePasajero} ${apellidoPasajero}`; //cadena de carácteres donde se interporla, se maneja como plantilla de texto 
+console.log(nombreCompleto);
+
+let segundoPasajero= "Diego Castillo";
+console.log (`El nombre del segundo Pasajero es: ${segundoPasajero}`);
+//por ejemplo si colocamos lo siguiente JS no dará error, hará la conversión de string a number
