0c9ee05373f6471ea126f14d4e1dea69c59d60f2
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-08 15:27:23 -0600

    se realizo ejercicio con conficionales encadenadas

--- 11-Operadoreslogicosencadenados.js
+++ 11-Operadoreslogicosencadenados.js
@@ -1,20 +1,23 @@
 
 const ciudadDestino = "Lima";
 const ciudadesDisponibles = new Array("Bogota", "Lima", "Santiago", "Montevideo");
-let edadPasajero = 17;
+let edadPasajero = 18;
 let estaAcompañado = true;
 let tienePasapporte = true;
+let estaCasado= false;
 
 
 
-// A && B || C
 console.log(`Verificando pasajes para  ${ciudadDestino}`);
+// A && B || C
+
 if (ciudadesDisponibles.indexOf(ciudadDestino) > -1  &&
    edadPasajero >= 18 &&
-   tienePasapporte) {
-  console.log("Pasaje disponible para venta");
+   tienePasapporte &&
+   !estaCasado) {
+  console.log("Paquete para solteros disponibles para venta");
 } else {
-    console.log ("Ciudad No disponible para viajar or pasajero no cumple las reglas");
+    console.log ("Ciudad No disponible para viajar o pasajero no cumple las reglas");
 } //forma para que se ejecuten las 3 condiciones 
 
 
